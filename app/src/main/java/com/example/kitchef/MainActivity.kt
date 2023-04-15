2e54a5687624932cd663546c879877e46fbe3723
jenniferhuang <jenniferhuang.jo@gmail.com>
2023-04-14 23:02:10 -0400

    add top bar and bottom navbar for each page

--- app/src/main/java/com/example/kitchef/MainActivity.kt
+++ app/src/main/java/com/example/kitchef/MainActivity.kt
@@ -1,54 +1,34 @@
 package com.example.kitchef
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
-import android.widget.SearchView
 import androidx.navigation.findNavController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
-import com.example.kitchef.common.StaticIngredientList
 import com.example.kitchef.databinding.ActivityMainBinding
-import com.example.kitchef.presentation.ui.home.AddIngredientViewModelFactory
-import com.example.kitchef.presentation.ui.home.AddIngredientsViewModel
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.closestKodein
-import org.kodein.di.generic.instance
 
 class MainActivity : AppCompatActivity(), KodeinAware {
 
     override val kodein by closestKodein()
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setUpBottomNavigation()
-//        handleSearching()
     }
 
     private fun setUpBottomNavigation() {
-        val navFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         NavigationUI.setupWithNavController(binding.homeBottomNavbar, navFragment.navController)
-
-        navFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
-            when(destination.id) {
-                R.id.homeFragment -> setBottomNavVisibility(View.VISIBLE)
-                R.id.favoriteFragment -> setBottomNavVisibility(View.VISIBLE)
-                R.id.settingFragment -> setBottomNavVisibility(View.VISIBLE)
-                else -> setBottomNavVisibility(View.GONE)
-            }
-        }
-    }
-
-    private fun setBottomNavVisibility(visibility: Int) {
-        binding.homeBottomNavbar.visibility = visibility
     }
 
 //    private fun handleSearching() {
